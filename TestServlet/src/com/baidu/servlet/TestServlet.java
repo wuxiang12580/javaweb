94f89cb0bee072bb370edd8f1a61dd52388ad742
wu <william.wu@shein.com>
2018-11-15 15:24:13 +0800

    edit

---
+++ TestServlet/src/com/baidu/servlet/TestServlet.java
@@ -0,0 +1,32 @@
+package com.baidu.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class TestServlet implements Servlet {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("testServlet……");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
